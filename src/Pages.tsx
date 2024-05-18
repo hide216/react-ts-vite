b0e1b3920a660c9e096c1b0f1dbf1daec21ab8ea
h-miyake <h-miyake@outlook.jp>
2024-05-18 18:48:46 +0900

    React Routeを追加しSPA化する

---
+++ src/Pages.tsx
@@ -0,0 +1,71 @@
+export function Home() {
+  return (
+    <>
+      <div>
+        <h1>Home</h1>
+        <div className="card">
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  );
+}
+
+export function About() {
+  return (
+    <>
+      <div>
+        <h1>About</h1>
+        <div className="card">
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  );
+}
+
+export function Events() {
+  return (
+    <>
+      <div>
+        <h1>Events</h1>
+        <div className="card">
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  );
+}
+
+export function Todo() {
+  return (
+    <>
+      <div>
+        <h1>Todo</h1>
+        <div className="card">
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  );
+}
